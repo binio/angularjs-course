b734353639cb616758b6a62e2288b380b804ddaf
binio <tbiniecki@gmail.com>
2018-01-06 23:14:51 +0000

    remove routes comment

--- src/section-4-routing/controllers/routes.js
+++ src/section-4-routing/controllers/routes.js
@@ -4,10 +4,7 @@
             controller: 'CustomersController',
             templateUrl: 'views/customers.html'
         }).otherwise({redirectTo: '/'});
-        //     .when('/orders',{
-        //     controller: 'OrdersController',
-        //     templateUrl: 'app/views/orders.html'
-        // }).otherwise({redirectTo: '/'});
+
     });
 
 })();
