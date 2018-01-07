434000c9e1486d46eea039be3298c938bab3fdb9
binio <tbiniecki@gmail.com>
2018-01-07 18:48:41 +0000

    add orders data

--- src/section-4-routing/controllers/CustomerController.js
+++ src/section-4-routing/controllers/CustomerController.js
@@ -4,10 +4,26 @@
         $scope.sortBy = 'name';
         $scope.reverse = false;
         $scope.customers = [
-            {joined: '2000-12-02', name: 'John', city: 'Chandler', orderTotal: 9.9956},
-            {joined: '1965-01-25', name: 'Zed', city: 'Las Vegas', orderTotal: 19.99},
-            {joined: '1944-06-15', name: 'Tina', city: 'New York', orderTotal: 44.99},
-            {joined: '1995-03-28', name: 'Dave', city: 'Seattle', orderTotal: 101.50}
+            {joined: '2000-12-02', name: 'John', city: 'Chandler', orderTotal: 9.9956, orders:[
+                    {id:1,product:'product 1', total:123},
+                    {id:1,product:'product 1', total:123},
+                    {id:1,product:'product 1', total:123} ]
+            },
+            {joined: '1965-01-25', name: 'Zed', city: 'Las Vegas', orderTotal: 19.99, orders:[
+                    {id:1,product:'product 12', total:123},
+                    {id:1,product:'product 12', total:123},
+                    {id:1,product:'product 12', total:123} ]
+            },
+            {joined: '1944-06-15', name: 'Tina', city: 'New York', orderTotal: 44.99, orders:[
+                    {id:1,product:'product 13', total:123},
+                    {id:1,product:'product 13', total:123},
+                    {id:1,product:'product 13', total:123} ]
+            },
+            {joined: '1995-03-28', name: 'Dave', city: 'Seattle', orderTotal: 101.50, orders:[
+                    {id:1,product:'product 14', total:123},
+                    {id:1,product:'product 14', total:123},
+                    {id:1,product:'product 14', total:123} ]
+            }
         ];
         $scope.doSort = function (propName) {
             $scope.sortBy = propName;
