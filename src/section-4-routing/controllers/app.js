b1a048fdd344bf63321a8c3bb05b3e04f4c3b5c3
binio <tbiniecki@gmail.com>
2018-01-07 20:42:21 +0000

    add config part for router prefix

--- src/section-4-routing/controllers/app.js
+++ src/section-4-routing/controllers/app.js
@@ -1,1 +1,6 @@
 var app = angular.module('customersApp',['ngRoute']);
+// (function() {
+//     app.config(['$locationProvider', function ($locationProvider) {
+//         $locationProvider.hashPrefix('');
+//     }]);
+// })();
