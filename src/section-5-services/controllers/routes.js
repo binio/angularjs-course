0a80c70956291a3efe091e7d27097525b9e9d59d
binio <tbiniecki@gmail.com>
2018-01-08 23:42:14 +0000

    add routes body

---
+++ src/section-5-services/controllers/routes.js
@@ -0,0 +1,13 @@
+(function() {
+    app.config(function ($routeProvider) {
+        $routeProvider.when('/', {
+            controller: 'CustomersController',
+            templateUrl: 'views/customers.html'
+        }).when('/orders/:customerId', {
+            controller: 'OrdersController',
+            templateUrl: 'views/orders.html'
+        }).otherwise({redirectTo: 'http://wp.pl'});
+
+    });
+
+})();
