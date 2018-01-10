0f569d3fbc7e9f6b355ab6553b83cd9b63efbc4a
binio <tbiniecki@gmail.com>
2018-01-10 22:46:52 +0000

    clean up

--- src/section-5-services/controllers/OrdersController.js
+++ src/section-5-services/controllers/OrdersController.js
@@ -1,40 +1,11 @@
 (function(){
-    OrdersController.$inject = ['$scope','$routeParams'];
-    function OrdersController($scope, $routeParams){
-        $scope.customers = [
-            {id:1,joined: '2000-12-02', name: 'John', city: 'Chandler', orderTotal: 9.9956, orders:[
-                    {id:1,product:'product 1', total:123},
-                    {id:1,product:'product 1', total:123},
-                    {id:1,product:'product 1', total:123} ]
-            },
-            {id:2,joined: '1965-01-25', name: 'Zed', city: 'Las Vegas', orderTotal: 19.99, orders:[
-                    {id:1,product:'product 12', total:123},
-                    {id:1,product:'product 12', total:123},
-                    {id:1,product:'product 12', total:123} ]
-            },
-            {id:3,joined: '1944-06-15', name: 'Tina', city: 'New York', orderTotal: 44.99, orders:[
-                    {id:1,product:'product 13', total:123},
-                    {id:1,product:'product 13', total:123},
-                    {id:1,product:'product 13', total:123} ]
-            },
-            {id:4,joined: '1995-03-28', name: 'Dave', city: 'Seattle', orderTotal: 101.50, orders:[
-                    {id:1,product:'product 14', total:123},
-                    {id:1,product:'product 14', total:123},
-                    {id:1,product:'product 14', total:123} ]
-            }
-        ];
+    OrdersController.$inject = ['$scope','$routeParams','customersFactory'];
+    function OrdersController($scope, $routeParams, customersFactory){
+        $scope.customer = null;
         var customerId = $routeParams.customerId;
-        $scope.orders = null;//$scope.customers[0].orders;
 
         function init(){
-            console.log('init called');
-            for(var i =0, len=$scope.customers.length; i<len; i++){
-
-                if($scope.customers[i].id === parseInt(customerId)){
-                    $scope.orders = $scope.customers[i].orders;
-                    break;
-                }
-            }
+            $scope.customer = customersFactory.getCustomer(customerId);
         };
 
 
