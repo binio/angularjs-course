1aee2a8efc7c4eed19db00099631cfe7cace27e1
binio <tbiniecki@gmail.com>
2018-01-10 22:29:13 +0000

    create factory

---
+++ src/section-5-services/services/customersFactory.js
@@ -0,0 +1,34 @@
+(function(){
+    var customersFactory = function(){
+        var customers =  [
+            {id:1, joined: '2000-12-02', name: 'John', city: 'Chandler', orderTotal: 9.9956, orders:[
+                    {id:1,product:'product 1', total:123},
+                    {id:1,product:'product 1', total:123},
+                    {id:1,product:'product 1', total:123} ]
+            },
+            {id:2,joined: '1965-01-25', name: 'Zed', city: 'Las Vegas', orderTotal: 19.99, orders:[
+                    {id:1,product:'product 12', total:123},
+                    {id:1,product:'product 12', total:123},
+                    {id:1,product:'product 12', total:123} ]
+            },
+            {id:3,joined: '1944-06-15', name: 'Tina', city: 'New York', orderTotal: 44.99, orders:[
+                    {id:1,product:'product 13', total:123},
+                    {id:1,product:'product 13', total:123},
+                    {id:1,product:'product 13', total:123} ]
+            },
+            {id:4,joined: '1995-03-28', name: 'Dave', city: 'Seattle', orderTotal: 101.50, orders:[
+                    {id:1,product:'product 14', total:123},
+                    {id:1,product:'product 14', total:123},
+                    {id:1,product:'product 14', total:123} ]
+            }
+        ];
+
+        var factory = {};
+        factory.getCustomers = function(){
+            return customers;
+        };
+
+        return factory;
+    };
+    app.factory('customersFactory', customersFactory);
+})();
