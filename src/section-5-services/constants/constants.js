28ec819a01e6781174e20d1470db6be4c0f0d5d6
binio <tbiniecki@gmail.com>
2018-01-12 22:40:57 +0000

    add constants

---
+++ src/section-5-services/constants/constants.js
@@ -0,0 +1,8 @@
+(function(){
+    app.constant('appName', 'CustomersApp by B1N10');
+    app.constant('appSettings', {
+        "version":"1.0",
+        "locale":"en-gb",
+        "timeout":180
+    });
+})();
