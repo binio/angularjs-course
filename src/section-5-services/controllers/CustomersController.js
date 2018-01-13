ff4f1d87dd9dd68d41330e8e10ea97578340773e
binio <tbiniecki@gmail.com>
2018-01-13 20:30:23 +0000

    extract data from server results

--- src/section-5-services/controllers/CustomersController.js
+++ src/section-5-services/controllers/CustomersController.js
@@ -1,13 +1,23 @@
 (function(){
-    CustomersController.$inject = ['$scope', 'customersService','appName'];
-    function CustomersController($scope, customersService, appName){
+    CustomersController.$inject = ['$scope', 'customersFactory','appName'];
+    function CustomersController($scope, customersFactory, appName){
         $scope.sortBy = 'name';
         $scope.reverse = false;
         $scope.customers = [];
         $scope.appName = appName;
 
         function init(){
-            $scope.customers = customersService.getCustomers();
+            customersFactory.getCustomers()
+                .then(
+                    function(customers){
+                        $scope.customers = customers.data;
+                        console.log(customers.data);
+                    },
+                    function(data, status, header, config){
+                        //todo
+                    });
+
+
         };
         init();
         $scope.doSort = function (propName) {
